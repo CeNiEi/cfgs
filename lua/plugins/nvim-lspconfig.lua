2e7947841135944c83aa894a4fdf38da19472e36
CeNiEi <ryujinjakka216@gmail.com>
2025-04-02 01:07:01 +0530

    stuff

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -22,20 +22,13 @@ return {
             'williamboman/mason-lspconfig.nvim',
             opts = {
                 ensure_installed = {
-                    "rust_analyzer", "lua_ls", "clangd", "hls", "zls"
+                    "rust_analyzer", "lua_ls", "clangd", "hls", "zls", "taplo"
                 },
                 handlers = {
 
                     ["rust_analyzer"] = function()
                         require("lspconfig")["rust_analyzer"].setup {
                             capabilities = capabilities,
-                            settings = {
-                                ["rust-analyzer"] = {
-                                    cargo = {
-                                        allFeatures = true,
-                                    },
-                                },
-                            },
                         }
                     end,
                     ["lua_ls"] = function()
@@ -100,6 +93,16 @@ return {
                 border = "rounded",
             }
         )
+
+        for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+            local default_diagnostic_handler = vim.lsp.handlers[method]
+            vim.lsp.handlers[method] = function(err, result, context, config)
+                if err ~= nil and err.code == -32802 then
+                    return
+                end
+                return default_diagnostic_handler(err, result, context, config)
+            end
+        end
 
         vim.api.nvim_create_autocmd('LspAttach', {
             group = vim.api.nvim_create_augroup('UserLspConfig', {}),
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,6 +4,9 @@ return {
         require("oil").setup {
             keymaps = {
                 ["<C-h>"] = false,
+            },
+            view_options = {
+                show_hidden = true
             }
         }
         vim.keymap.set("n", "-", "<CMD>Oil --float<CR>")
