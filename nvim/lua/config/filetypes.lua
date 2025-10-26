a16f297d40d31de11b795dacfc79865f4d82c42a
CeNiEi <ryujinjakka216@gmail.com>
2025-10-26 12:35:12 +0530

    feat(nvim): add local opts for wgsl ft

--- nvim/lua/config/filetypes.lua
+++ nvim/lua/config/filetypes.lua
@@ -3,3 +3,13 @@ vim.filetype.add({
 		wgsl = "wgsl",
 	},
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "wgsl",
+	callback = function()
+		vim.opt_local.expandtab = true
+		vim.opt_local.shiftwidth = 4
+		vim.opt_local.softtabstop = 4
+		vim.opt_local.commentstring = "// %s"
+	end,
+})
